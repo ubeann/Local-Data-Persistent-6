1ff28a98da1df21ae5e0fa447ecaa760c24603b7
Ubean <ubeannn@gmail.com>
2021-11-28 00:34:19 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/local_data_persistent_6/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.local_data_persistent_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
